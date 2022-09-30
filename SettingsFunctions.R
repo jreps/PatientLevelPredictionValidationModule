d9798f3986bd58397da2e51e09c494330b7f8874
Jenna Reps <jreps@its.jnj.com>
2022-09-30 13:31:57 -0400

    added active.R
    - added active.R in renv folder

--- SettingsFunctions.R
+++ SettingsFunctions.R
@@ -11,7 +11,7 @@ createPatientLevelPredictionValidationModuleSpecifications <- function(
   
   specifications <- list(
     module = "PatientLevelPredictionValidationModule",
-    version = "0.0.3",
+    version = "0.0.4",
     remoteRepo = "github.com",
     remoteUsername = "jreps",
     settings = list(
